2a0ff9c82d90e262db1850cd20faf7b03ce3bacc
yangjh-xbmu <yangjh@yeah.net>
2023-09-07 22:23:18 +0800

    自动推送

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
-git config --global user.name 'echo-2023chess/luxuriant'
+git config --global user.name 'echo-2023chess'
 git config --global user.email '3232748922@qq.com'
 git config --global http.sslverify false
-git add .
+git add.
 git commit -m '自动推送'
 git push
